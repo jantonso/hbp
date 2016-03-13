088f07f04c797cde6889d273815ddb55b8bd11ec
jantonso <jantonso@andrew.cmu.edu>
2016-03-13 19:19:12 -0400

    Finished rough version of personalized care page

--- hbp/urls.py
+++ hbp/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url(r'^$', views.index, name='index'),
+	url(r'^pc/', views.personalizedCare, name='personalizedCare'),
 )
--- hbp/views.py
+++ hbp/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'index.html', {})
+
+def personalizedCare(request):
+	if request.method == 'GET':
+		return render(request, 'personalizedCare.html', {})
+	elif request.method == 'POST':
+		# Do POST SHIT => redirect to important information
+		return render(request, 'importantInformation.html', {})
+
+ 
