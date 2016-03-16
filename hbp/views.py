3d0c084bd6a43c7725aad8eb660de6d7684b883d
jantonso <jantonso@andrew.cmu.edu>
2016-03-16 14:55:24 -0400

    Added session to keep track of whether the user has finished the required videos

--- hbp/views.py
+++ hbp/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 
 from django.shortcuts import render, redirect
 
+# The index that indicates the user has finished all the videos
+finished_videos_index = 3
+
 def index(request):
 	return render(request, 'index.html', {})
 
@@ -9,13 +12,27 @@ def personalizedCare(request):
 	if request.method == 'GET':
 		return render(request, 'personalizedCare.html', {})
 	elif request.method == 'POST':
+		# Create session to keep track of if the user has watched all their required videos
+		request.session['finished_videos'] = False
 		# Do POST SHIT => redirect to important information
 		return redirect('/iv/')
 
 def informationalVideos(request, video_index=0):
-	try:
+	try:		
+		finished_videos = False
+		# Check to see if they have finished all their videos
+		if (request.session.get('finished_videos', False)):
+			finished_videos = True
+
 		video_index = int(video_index)
-		return render(request, 'importantInformation.html', {'video_index': video_index})
+		if (not finished_videos):
+			# Update the session to reflect that they have finished all the videos
+			if (video_index == finished_videos_index):
+				request.session['finished_videos'] = True
+				finished_videos = True
+
+		return render(request, 'importantInformation.html', {'video_index': video_index, 
+						'finished_videos': finished_videos})
 	except ValueError as e:
 		print e
 
