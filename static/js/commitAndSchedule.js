138470e6e737a9988d313446d8ab634ea7a35441
jantonso <jantonso@andrew.cmu.edu>
2016-03-19 18:06:58 -0400

    Added dob validation and finished commit and schedule page

--- static/js/commitAndSchedule.js
+++ static/js/commitAndSchedule.js
@@ -1,17 +1,94 @@
 var MIN_YEAR = 1900;
 var CURRENT_YEAR = 2016;
 
-function checkDate() {
+// -------------------------------- 1: Commit -----------------------------------------
+
+// Hide the commit  section, show the delivery date section
+function onCommitClick() {
+	$('#cs-commit').hide();
+	$('#cs-delivery-date').show();
+	$('#confirm-date-button').show();
+}
+
+// -------------------------------- 2: Delivery Date ----------------------------------
+
+function checkDeliveryDate() {
 	var day = document.getElementById('deliveryDay').value;
 	var month = document.getElementById('deliveryMonth').value;
 	var year = document.getElementById('deliveryYear').value;
 	if (validateDate(day, month, year)) {
+		// Store the delivery date in a new field
 		var date = month + '/' + day + '/' + year;
-		dateIsValid(date);
+		console.log('Date is valid!');
+		$('#deliveryDate').value = date;
+
+		// Hide the delivery date section, show the calendar section
+		$('#cs-delivery-date').hide();
+		$('#confirm-date-button').hide();
+		$('#cs-calendar').show();
+		$('#confirm-appt-button').show();
 	} else {
-		dateIsInvalid();
+		// Show error message
+		console.log('Please enter a valid date...')
+		$('#delivery-date-error-msg').text('That date is invalid. Please try again.');
+		$('#delivery-date-error-msg').show();
 	}
 }
+
+// -------------------------------- 3: Calendar ---------------------------------------
+
+// Hide the calendar section, show the signature section
+function getSignature() {
+	$('#cs-calendar').hide();
+	$('#confirm-appt-button').hide();
+	$('#cs-signature').show();
+	$('#confirm-sig-button').show();
+}
+
+// -------------------------------- 4: Signature --------------------------------------
+
+function validateSigForm() {
+	if (checkName() && checkDOBDate()) {
+		return true
+	} else {
+		return false;
+	}
+}
+
+// Make sure the name field is valid
+function checkName() {
+	var name = document.getElementById('sig-name').value;
+	if (name != null && name != '') {
+		return true;
+	} else {
+		console.log('Please enter your name.');
+		$('#dob-error-msg').text('Please enter your name.');
+		$('#dob-error-msg').show();
+		return false;
+	}
+}
+
+// Make sure the date of birth field is valid
+function checkDOBDate() {
+	var day = document.getElementById('dobDay').value;
+	var month = document.getElementById('dobMonth').value;
+	var year = document.getElementById('dobYear').value;
+	if (validateDate(day, month, year)) {
+		// Store value in a new dob field
+		console.log('Date is valid!');
+		var date = month + '/' + day + '/' + year;
+		$('#dobDate').value = date;
+		return true;
+	} else {
+		// Display error message
+		console.log('Please enter a valid date...')
+		$('#dob-error-msg').text('That date is invalid. Please try again.');
+		$('#dob-error-msg').show();
+		return false;
+	}
+}
+
+// -------------------------------- Helpers -------------------------------------------
 
 // Check to make sure that the date is valid
 function validateDate(day, month, year) {
@@ -52,35 +129,4 @@ function validateDate(day, month, year) {
 	}
 
 	return true;
-}
-
-function dateIsInvalid() {
-	console.log('Please enter a valid date...')
-	$('#error-msg').show();
-}
-
-function dateIsValid(date) {
-	console.log('Date is valid!');
-	$('#deliveryDate').value = date;
-
-	// Hide the delivery date section, show the calendar section
-	$('#cs-delivery-date').hide();
-	$('#confirm-date-button').hide();
-	$('#cs-calendar').show();
-	$('#confirm-appt-button').show();
-}
-
-// Hide the commit message section, show the delivery date section
-function onCommitClick() {
-	$('#cs-commit').hide();
-	$('#cs-delivery-date').show();
-	$('#confirm-date-button').show();
-}
-
-// Hide the calendar section, show the signature section
-function getSignature() {
-	$('#cs-calendar').hide();
-	$('#confirm-appt-button').hide();
-	$('#cs-signature').show();
-	$('#confirm-sig-button').show();
 }
