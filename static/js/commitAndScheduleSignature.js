3849d6e69937899fa49c97282d472f4e124f4b12
jantonso <jantonso@andrew.cmu.edu>
2016-03-29 11:42:56 -0400

    Moved check date js code to its own file

--- static/js/commitAndScheduleSignature.js
+++ static/js/commitAndScheduleSignature.js
@@ -49,62 +49,6 @@ function checkDOBDate() {
 	}
 }
 
-// Make sure the user signed
-function checkSignature() {
-	if (signaturePad.isEmpty()) {
-		// Display error message
-		console.log('Please sign...');
-		$('#sig-error-msg').text('Please provide a signature.');
-		$('#sig-error-msg').show();
-		return false;
-	} else {
-		// save the image as png
-		//signaturePad.toDataURL();
-		return true;
-	}
-}
-
-// Check to make sure that the date is valid
-function checkDate(day, month, year) {
-	var isDay = /^[0-9]+$/.test(day);
-	var isMonth = /^[0-9]+$/.test(month);
-	var isYear = /^[0-9]+$/.test(year);
-
-	// Check that the string contains only digits
-	if (!isDay || !isMonth || !isYear) {
-		return false;		
-	}
-
-	var dayValue = parseInt(day);
-	var monthValue = parseInt(month);
-	var yearValue = parseInt(year);
-
-	// Check that the string can be converted to a number (should always be the case)
-	if (isNaN(day)|| isNaN(month) || isNaN(year)) {
-		return false;
-	}
-
-	// Check that the month is valid
-	if (monthValue < 1 || monthValue > 12) {
-		return false;
-	}
-
-	// Include leap year day by default... no extra checking for it
-	// Since we only care about ~6 weeks out from delivery date it is okay
-	var listOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	// Check that the day is valid
-	if (dayValue < 1 || dayValue > listOfDays[monthValue-1]) {
-		return false;
-	}
-
-	// Check that the year is valid
-	if (yearValue < MIN_YEAR || yearValue > CURRENT_YEAR) {
-		return false;
-	}
-
-	return true;
-}
-
 // Adjust canvas coordinate space taking into account pixel ratio,
 // to make it look crisp on mobile devices.
 function resizeCanvas() {
