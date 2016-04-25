421ceb4c3d5f99cd21dd98fe15f6dcef23587f3f
jantonso <jantonso@andrew.cmu.edu>
2016-04-25 16:15:34 -0400

    Adding more support for validating dates using moment js library

--- static/js/checkDate.js
+++ static/js/checkDate.js
@@ -1,43 +1,23 @@
-var MIN_YEAR = 1900;
-var CURRENT_YEAR = 2016;
+var dayFormats = ['D', 'DD'];
+var monthFormats = ['M', 'MM', 'MMM', 'MMMM'];
+var yearFormats = ['YYYY', 'YY'];
 
 // Check to make sure that the date is valid
 function checkDate(day, month, year) {
-	var isDay = /^[0-9]+$/.test(day);
-	var isMonth = /^[0-9]+$/.test(month);
-	var isYear = /^[0-9]+$/.test(year);
 
-	// Check that the string contains only digits
-	if (!isDay || !isMonth || !isYear) {
-		return false;		
+	var date = month + '/' + day + '/' + year;
+	for (var i = 0; i < dayFormats.length; i++) {
+		for (var j = 0; j < monthFormats.length; j++) {
+			for (var k = 0; k < yearFormats.length; k++) {
+				var dFmt = dayFormats[i];
+				var mFmt = monthFormats[j];
+				var yFmt = yearFormats[k];
+				if (moment(date, mFmt + '/' + dFmt + '/' + yFmt).isValid()) {
+					return true;
+				}
+			}
+		}
 	}
 
-	var dayValue = parseInt(day);
-	var monthValue = parseInt(month);
-	var yearValue = parseInt(year);
-
-	// Check that the string can be converted to a number (should always be the case)
-	if (isNaN(day)|| isNaN(month) || isNaN(year)) {
-		return false;
-	}
-
-	// Check that the month is valid
-	if (monthValue < 1 || monthValue > 12) {
-		return false;
-	}
-
-	// Include leap year day by default... no extra checking for it
-	// Since we only care about ~6 weeks out from delivery date it is okay
-	var listOfDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-	// Check that the day is valid
-	if (dayValue < 1 || dayValue > listOfDays[monthValue-1]) {
-		return false;
-	}
-
-	// Check that the year is valid
-	if (yearValue < MIN_YEAR || yearValue > CURRENT_YEAR) {
-		return false;
-	}
-
-	return true;
+	return false;
 }
