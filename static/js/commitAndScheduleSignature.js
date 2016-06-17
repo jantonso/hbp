cb4aae6f07d8b5bbc2d68c97c01ac974c16c184e
jantonso <jantonso@andrew.cmu.edu>
2016-06-17 13:46:02 -0400

    Changin size of sig pad for heroku2

--- static/js/commitAndScheduleSignature.js
+++ static/js/commitAndScheduleSignature.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	// Bring up the canvas to allow users to sign
 	canvas = document.getElementById('signature-canvas');
 	canvas.width = 800;
-	canvas.height = 260;
+	canvas.height = 230;
 	signaturePad = new SignaturePad(canvas);
 
 	window.onresize = resizeCanvas;
