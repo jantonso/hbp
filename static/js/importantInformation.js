a3ed6b04f1f512c7dec913094c1d34eccc15bd75
jantonso <jantonso@andrew.cmu.edu>
2016-08-03 18:33:44 -0400

    fixed loading indicator position

--- static/js/importantInformation.js
+++ static/js/importantInformation.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
 	$('.loading-message').hide();
 
 	$('footer .next-button').click(function() {
-		$('#videos-container iframe').hide();
-		$('#video-links .video-link').hide();
 		$('.loading-message').show();
 	});
 
 	$('footer .back-button').click(function() {
-		$('#videos-container iframe').hide();
+		$('.loading-message').css('float', 'left');
+		$('.loading-message').show();
+	});
+
+	$('#video-links .video-link').click(function() {
+		console.log("sup");
+		$('.loading-message').css('float', 'none');
+		$('.loading-message').css('margin-left', 'auto');
+		$('.loading-message').css('margin-right', 'auto');
 		$('.loading-message').show();
 	});
 });
